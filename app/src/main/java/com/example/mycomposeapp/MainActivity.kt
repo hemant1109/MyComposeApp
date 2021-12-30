ce2bb9b1aea602ac216a686545a6ae066a4bb114
Hemant <hasolanki1109@gmail.com>
2021-12-30 18:02:52 +0530

    First compose app commit

---
+++ app/src/main/java/com/example/mycomposeapp/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.mycomposeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mycomposeapp.ui.theme.MyComposeAppTheme
+import com.example.mycomposeapp.ui.theme.colors
+
+class MainActivity : ComponentActivity() {
+
+    @ExperimentalMaterialApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyComposeAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    //Greeting("Android")
+                    SimpleColumnList()
+                }
+            }
+        }
+    }
+}
+
+var editable by remember { mutableStateOf(true) }
+
+@Preview(showBackground = true)
+@ExperimentalMaterialApi
+@Composable
+fun SimpleColumnList() {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        modifier = Modifier
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        for (i in 1..30)
+
+            ListItem(
+                text = { Text(text = "Item $i") },
+                modifier = Modifier
+                    .background(color = colors[i % colors.size])
+                    .padding(8.dp)
+            )
+    }
+}
+
